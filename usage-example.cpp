dc9a6f32fca6058f21903e5864c432421f356a9b
Laurent Cabaret <laurent.cabaret@gmail.com>
2021-03-27 13:38:16 +0100

    Usage explanations

--- usage-example.cpp
+++ usage-example.cpp
@@ -1,10 +1,9 @@
-// C++ program to illustrate time point
-// and system clock functions
+// C++ program to illustrate the different usages
+// of the class chrone
 #include <chrono> //NOLINT
 #include <iostream>
 #include "chrone.hpp"
-// Function to calculate
-// Fibonacci series
+
 int64_t fibonacci(unsigned n) {
     if (n < 2) return n;
     return fibonacci(n - 1) + fibonacci(n-2);
@@ -12,8 +11,14 @@ int64_t fibonacci(unsigned n) {
 
 int main() {
     chrone mainChrone("main_monitor.csv");
+    // The chrone is created and can now hosts many timers
+    
     for (int i = 3; i < 42; ++i) {
         timer local_timer("Fibonacci", &mainChrone);
+        // Here the timer `local_timer` is created, initialized 
+        // with its creation timestamp and add to mainChrone
+        // that will export the result in `main_monitor.csv`
+        // when mainChrone is destroy (end of scope). 
         fibonacci(i);
-    }
+    } // each time `local_timer` is out of scope, its life duration is computed and saved in `chrone`.
 }
