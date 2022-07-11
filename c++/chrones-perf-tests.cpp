ee8ed1be3f9530833d4627de9bb6792c5c9a0736
Vincent Jacques <vincent@vincent-jacques.net>
2022-07-11 13:59:10 +0200

    Rewrite perf test to avoid measuring OpenMP setup time

--- c++/chrones-perf-tests.cpp
+++ c++/chrones-perf-tests.cpp
@@ -2,6 +2,7 @@
 // Copyright 2020-2022 Vincent Jacques
 
 #include <gtest/gtest.h>
+#include <omp.h>
 
 #include <chrono>  // NOLINT(build/c++11)
 #include <iostream>
@@ -101,23 +102,38 @@ TEST_F(HeavyChronesPerformanceTest, SequentialFull) {
 // [       OK ] HeavyChronesPerformanceTest.SequentialFull (7033 ms)
 
 TEST_F(HeavyChronesPerformanceTest, ParallelFull) {
-  for (int j = 0; j != 5; ++j) {
-    Timer timer;
+  const int threads = 8;
+  omp_set_num_threads(threads);
+  ASSERT_EQ(omp_get_num_threads(), 1);
 
-    #pragma omp parallel for
-    for (int i = 0; i != MILLION; ++i) {
-      heavy_stopwatch t(&c, __PRETTY_FUNCTION__, "label", i);
+  #pragma omp parallel
+  {
+    EXPECT_EQ(omp_get_num_threads(), threads);
+
+    for (int j = 0; j != 5; ++j) {
+      Timer timer;
+
+      for (int i = 0; i != MILLION / threads; ++i) {
+        heavy_stopwatch t(&c, __PRETTY_FUNCTION__, "label", i);
+      }
+
+      #pragma omp barrier
+      const auto d = timer.duration();
+      #pragma omp critical
+      {
+        std::cerr << std::chrono::nanoseconds(d).count() / 1e9 << "s ";
+      }
+      #pragma omp barrier
+      #pragma omp master
+      std::cerr << std::endl;
+      // @todo EXPECT_LE(d, std::chrono::seconds(1));
     }
-
-    const auto d = timer.duration();
-    std::cerr << std::chrono::nanoseconds(d).count() / 1e9 << "s" << std::endl;
-    // @todo EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
 // [ RUN      ] HeavyChronesPerformanceTest.ParallelFull
-// 0.770904s
-// 0.56584s
-// 0.561584s
-// 0.545847s
-// 0.565209s
-// [       OK ] HeavyChronesPerformanceTest.ParallelFull (6134 ms)
+// 0.804436s 0.804436s 0.804443s 0.804436s 0.804484s 0.804447s 0.804533s 0.804534s
+// 0.804883s 0.804889s 0.804891s 0.804889s 0.804888s 0.804928s 0.804888s 0.804998s
+// 0.821919s 0.822003s 0.822003s 0.821998s 0.822004s 0.822003s 0.822004s 0.822014s
+// 0.816145s 0.816144s 0.816154s 0.816194s 0.816192s 0.816281s 0.816281s 0.816282s
+// 0.818835s 0.818839s 0.818835s 0.818835s 0.81884s 0.818835s 0.818841s 0.818913s
+// [       OK ] HeavyChronesPerformanceTest.ParallelFull (7375 ms)
