jacquev6/Chrones

commit 01bde66a70acb585344d030252f2327a2a89157b

Automate archiving of the performance tests results

Makefile
@@ -22,7 +22,7 @@ object_files := $(patsubst %.cpp,build/%.o,$(c++_source_files))
 
 # Sentinel files
 cpplint_sentinel_files := $(patsubst %,build/%.cpplint.ok,$(c++_header_files) $(c++_source_files))
-test_sentinel_files := $(patsubst %,build/%.tests.ok,$(c++_test_source_files)) build/chrones-report.py.tests.ok
+test_sentinel_files := $(patsubst %,build/%.tests.ok,$(c++_test_source_files)) build/chrones-report.py.tests.ok c++/chrones-perf-tests.log
 
 .PHONY: debug-inventory
 debug-inventory:
@@ -79,7 +79,7 @@ build/%-tests.cpp.tests.ok: build/%-tests
 	@echo "$<"
 	@mkdir -p $(dir $@)
 	@rm -f build/$*-tests.*.chrones.csv
-	@cd build/c++ && OMP_NUM_THREADS=4 ../../$<
+	@cd build/c++ && OMP_NUM_THREADS=4 ../../$< 2>&1 | tee ../../$@.log
 	@touch $@
 
 build/chrones-report.py.tests.ok: chrones-report.py build/c++/chrones-tests.cpp.tests.ok c++/chrones-tests.py
@@ -89,6 +89,9 @@ build/chrones-report.py.tests.ok: chrones-report.py build/c++/chrones-tests.cpp.
 	@./chrones-report.py summaries build/c++/chrones-tests.*.chrones.csv >build/c++/chrones-tests.chrones.summaries.json
 	@c++/chrones-tests.py build/c++/chrones-tests.chrones.summaries.json
 	@touch $@
+
+c++/chrones-perf-tests.log: build/c++/chrones-perf-tests.cpp.tests.ok
+	@cp $^.log $@
 
 ########
 # Link #

c++/chrones-perf-tests.cpp
@@ -79,13 +79,6 @@ TEST_F(HeavyChronesPerformanceTest, SequentialPlain) {
     EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
-// [ RUN      ] HeavyChronesPerformanceTest.SequentialPlain
-// 0.340938s
-// 0.306131s
-// 0.317522s
-// 0.440938s
-// 0.4058s
-// [       OK ] HeavyChronesPerformanceTest.SequentialPlain (5290 ms)
 
 TEST_F(HeavyChronesPerformanceTest, SequentialLabelled) {
   for (int j = 0; j != REPETITIONS; ++j) {
@@ -100,13 +93,6 @@ TEST_F(HeavyChronesPerformanceTest, SequentialLabelled) {
     EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
-// [ RUN      ] HeavyChronesPerformanceTest.SequentialLabelled
-// 0.267886s
-// 0.395312s
-// 0.447362s
-// 0.32896s
-// 0.342768s
-// [       OK ] HeavyChronesPerformanceTest.SequentialLabelled (5372 ms)
 
 TEST_F(HeavyChronesPerformanceTest, SequentialFull) {
   for (int j = 0; j != REPETITIONS; ++j) {
@@ -121,13 +107,6 @@ TEST_F(HeavyChronesPerformanceTest, SequentialFull) {
     EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
-// [ RUN      ] HeavyChronesPerformanceTest.SequentialFull
-// 0.24581s
-// 0.406601s
-// 0.284519s
-// 0.320636s
-// 0.326781s
-// [       OK ] HeavyChronesPerformanceTest.SequentialFull (5260 ms)
 
 TEST_F(HeavyChronesPerformanceTest, ParallelFull) {
   omp_set_num_threads(THREADS);
@@ -157,13 +136,6 @@ TEST_F(HeavyChronesPerformanceTest, ParallelFull) {
     }
   }
 }
-// [ RUN      ] HeavyChronesPerformanceTest.ParallelFull
-// 0.63508s 0.635081s 0.635081s 0.635087s 0.635127s 0.635227s 0.635224s 0.635235s
-// 0.573015s 0.573015s 0.573025s 0.573013s 0.573023s 0.573067s 0.573065s 0.573118s
-// 0.558125s 0.558132s 0.558125s 0.558173s 0.558125s 0.558251s 0.558252s 0.558251s
-// 0.586375s 0.586382s 0.586383s 0.586375s 0.586378s 0.58645s 0.58642s 0.586375s
-// 0.56918s 0.56918s 0.56919s 0.569185s 0.569188s 0.569227s 0.56928s 0.569284s
-// [       OK ] HeavyChronesPerformanceTest.ParallelFull (5863 ms)
 
 
 class LightChronesPerformanceTest : public testing::Test {
@@ -205,13 +177,6 @@ TEST_F(LightChronesPerformanceTest, SequentialPlain) {
     EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
-// [ RUN      ] LightChronesPerformanceTest.SequentialPlain
-// 0.106611s
-// 0.106607s
-// 0.107645s
-// 0.106317s
-// 0.108013s
-// [       OK ] LightChronesPerformanceTest.SequentialPlain (626 ms)
 
 TEST_F(LightChronesPerformanceTest, SequentialLabelled) {
   for (int j = 0; j != REPETITIONS; ++j) {
@@ -226,13 +191,6 @@ TEST_F(LightChronesPerformanceTest, SequentialLabelled) {
     EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
-// [ RUN      ] LightChronesPerformanceTest.SequentialLabelled
-// 0.127809s
-// 0.12759s
-// 0.13071s
-// 0.127654s
-// 0.130021s
-// [       OK ] LightChronesPerformanceTest.SequentialLabelled (741 ms)
 
 TEST_F(LightChronesPerformanceTest, SequentialFull) {
   for (int j = 0; j != REPETITIONS; ++j) {
@@ -247,13 +205,6 @@ TEST_F(LightChronesPerformanceTest, SequentialFull) {
     EXPECT_LE(d, std::chrono::seconds(1));
   }
 }
-// [ RUN      ] LightChronesPerformanceTest.SequentialFull
-// 0.135067s
-// 0.127967s
-// 0.12754s
-// 0.126688s
-// 0.128905s
-// [       OK ] LightChronesPerformanceTest.SequentialFull (731 ms)
 
 TEST_F(LightChronesPerformanceTest, ParallelFull) {
   omp_set_num_threads(THREADS);
@@ -283,10 +234,3 @@ TEST_F(LightChronesPerformanceTest, ParallelFull) {
     }
   }
 }
-// [ RUN      ] LightChronesPerformanceTest.ParallelFull
-// 0.627004s 0.627009s 0.627074s 0.6271s 0.627005s 0.627053s 0.627123s 0.627139s
-// 0.473635s 0.473635s 0.473636s 0.473635s 0.473638s 0.47364s 0.473681s 0.473677s
-// 0.502704s 0.502707s 0.502711s 0.502705s 0.502704s 0.502718s 0.502705s 0.50275s
-// 0.464927s 0.464932s 0.464931s 0.464927s 0.464924s 0.464927s 0.464927s 0.464933s
-// 0.461055s 0.461057s 0.461057s 0.461056s 0.461057s 0.46106s 0.461101s 0.461174s
-// [       OK ] LightChronesPerformanceTest.ParallelFull (2643 ms)

c++/chrones-perf-tests.log
@@ -1,0 +1,68 @@
+Running main() from /googletest-release-1.11.0/googletest/src/gtest_main.cc
+[==========] Running 8 tests from 2 test suites.
+[----------] Global test environment set-up.
+[----------] 4 tests from HeavyChronesPerformanceTest
+[ RUN      ] HeavyChronesPerformanceTest.SequentialPlain
+0.349628s
+0.307921s
+0.339546s
+0.443784s
+0.420108s
+[       OK ] HeavyChronesPerformanceTest.SequentialPlain (5450 ms)
+[ RUN      ] HeavyChronesPerformanceTest.SequentialLabelled
+0.206879s
+0.224536s
+0.417191s
+0.2753s
+0.330727s
+[       OK ] HeavyChronesPerformanceTest.SequentialLabelled (5175 ms)
+[ RUN      ] HeavyChronesPerformanceTest.SequentialFull
+0.234997s
+0.224314s
+0.41611s
+0.289814s
+0.332374s
+[       OK ] HeavyChronesPerformanceTest.SequentialFull (5398 ms)
+[ RUN      ] HeavyChronesPerformanceTest.ParallelFull
+0.643546s 0.643553s 0.643546s 0.643594s 0.643664s 0.643666s 0.643676s 0.643666s 
+0.596382s 0.5964s 0.596447s 0.596502s 0.596504s 0.596503s 0.596502s 0.596503s 
+0.580235s 0.580235s 0.580243s 0.580235s 0.580244s 0.580283s 0.580312s 0.580323s 
+0.634719s 0.634719s 0.634718s 0.634728s 0.634768s 0.634842s 0.634844s 0.634858s 
+0.608975s 0.608975s 0.608975s 0.608982s 0.609021s 0.609088s 0.609095s 0.609112s 
+[       OK ] HeavyChronesPerformanceTest.ParallelFull (6025 ms)
+[----------] 4 tests from HeavyChronesPerformanceTest (22050 ms total)
+
+[----------] 4 tests from LightChronesPerformanceTest
+[ RUN      ] LightChronesPerformanceTest.SequentialPlain
+0.112339s
+0.112311s
+0.112867s
+0.11293s
+0.113721s
+[       OK ] LightChronesPerformanceTest.SequentialPlain (637 ms)
+[ RUN      ] LightChronesPerformanceTest.SequentialLabelled
+0.134872s
+0.134491s
+0.13487s
+0.134333s
+0.136021s
+[       OK ] LightChronesPerformanceTest.SequentialLabelled (738 ms)
+[ RUN      ] LightChronesPerformanceTest.SequentialFull
+0.132409s
+0.132048s
+0.133366s
+0.133652s
+0.135131s
+[       OK ] LightChronesPerformanceTest.SequentialFull (734 ms)
+[ RUN      ] LightChronesPerformanceTest.ParallelFull
+0.555172s 0.555141s 0.555076s 0.555081s 0.555079s 0.555218s 0.555199s 0.555198s 
+0.472029s 0.472036s 0.472029s 0.472033s 0.472029s 0.472076s 0.472077s 0.47213s 
+0.438104s 0.438113s 0.438111s 0.438111s 0.438149s 0.43822s 0.438222s 0.438221s 
+0.442894s 0.442897s 0.442894s 0.442894s 0.442894s 0.442939s 0.44303s 0.44303s 
+0.47678s 0.476789s 0.47679s 0.476778s 0.476789s 0.47683s 0.47683s 0.476861s 
+[       OK ] LightChronesPerformanceTest.ParallelFull (2436 ms)
+[----------] 4 tests from LightChronesPerformanceTest (4547 ms total)
+
+[----------] Global test environment tear-down
+[==========] 8 tests from 2 test suites ran. (26597 ms total)
+[  PASSED  ] 8 tests.

commit de6772819a96dcccee28df51a4a4314cdb81b99a

Fix detection of failed tests (broken in 01bde66a70)

Makefile
@@ -1,6 +1,13 @@
 # Copyright 2020-2022 Laurent Cabaret
 # Copyright 2020-2022 Vincent Jacques
 
+# https://stackoverflow.com/a/52603343/905845
+MAKEFLAGS += --no-builtin-rules
+MAKEFLAGS += --no-builtin-variables
+
+# https://stackoverflow.com/a/589300/905845
+SHELL := /bin/bash -o pipefail -o errexit
+
 ############################
 # Default top-level target #
 ############################