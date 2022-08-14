f98340da3fb6bd10f12b84a11b5126a6b84ce1ef
Vincent Jacques <vincent@vincent-jacques.net>
2022-08-14 10:31:07 +0200

    Fix reporting of 'sw_summary' events in multi-processing case

--- chrones-report.py
+++ chrones-report.py
@@ -11,6 +11,7 @@ import dataclasses
 import functools
 import itertools
 import json
+import math
 import statistics
 import sys
 import textwrap
@@ -50,8 +51,9 @@ def summaries_command(args):
 
         - "function" (and optionally "label"): the identifiers of the stopwatch
         - "executions_count": the total number of times this stopwatch was run
-        - "average_duration_ms" (if executions_count > 1)
-        - "min_duration_ms", "median_duration_ms" and "max_duration_ms" (if executions_count > 1)
+        - "average_duration_ms", "duration_standard_deviation_ms" (if executions_count > 1)
+        - "min_duration_ms", "max_duration_ms" (if executions_count > 1)
+        - "median_duration_ms" (if executions_count > 1 and not multi-process MINICHRONE)
         - "total_duration_ms"
     """
     with open_events_file(args.events_file) as events:
@@ -236,6 +238,8 @@ class MakeEventTestCase(unittest.TestCase):
 
 
 def make_multi_process_summaries(events):
+    if not events:
+        return
     (all_durations, all_summaries) = functools.reduce(
         merge_durations_and_summaries,
         (
@@ -243,20 +247,126 @@ def make_multi_process_summaries(events):
             for _, process_events in itertools.groupby(events, key=lambda e: e.process_id)
         ),
     )
-    yield from all_summaries
+
+    for (key, summaries) in all_summaries.items():
+        if len(summaries) == 1:
+            summary = summaries[0]
+            yield Summary(
+                function_name=summary.function_name,
+                label=summary.label,
+                executions_count=summary.executions_count,
+                average_duration=summary.average_duration,
+                duration_standard_deviation=summary.duration_standard_deviation,
+                min_duration=summary.min_duration,
+                median_duration=summary.median_duration,
+                max_duration=summary.max_duration,
+                total_duration=summary.total_duration,
+            )
+        else:
+            assert len(summaries) > 1
+            executions_count = sum(s.executions_count for s in summaries)
+            yield Summary(
+                function_name=summaries[0].function_name,
+                label=summaries[0].label,
+                executions_count=executions_count,
+                average_duration=sum(s.executions_count * s.average_duration for s in summaries) / executions_count,
+                duration_standard_deviation=None,
+                min_duration=min(s.min_duration for s in summaries),
+                median_duration=None,
+                max_duration=max(s.max_duration for s in summaries),
+                total_duration=sum(s.total_duration for s in summaries),
+            )
+
     for (key, durations) in all_durations.items():
-        yield Summary(
-            key[0],
-            key[1],
-            len(durations),
-            statistics.mean(durations),
-            statistics.stdev(durations) if len(durations) > 1 else 0,
-            min(durations),
-            statistics.median(durations),
-            max(durations),
-            sum(durations),
-            # @todo (not needed by Laurent for now) Add summaries per process and per thread
+        if len(durations) > 1:
+            yield Summary(
+                function_name=key[0],
+                label=key[1],
+                executions_count=len(durations),
+                average_duration=statistics.mean(durations),
+                duration_standard_deviation=statistics.stdev(durations),
+                min_duration=min(durations),
+                median_duration=statistics.median(durations),
+                max_duration=max(durations),
+                total_duration=sum(durations),
+            )
+        else:
+            assert len(durations) == 1
+            yield Summary(
+                function_name=key[0],
+                label=key[1],
+                executions_count=1,
+                average_duration=None,
+                duration_standard_deviation=None,
+                min_duration=None,
+                median_duration=None,
+                max_duration=None,
+                total_duration=durations[0],
+            )
+
+
+class MakeMultiProcessSummariesTestCase(unittest.TestCase):
+    def make_multi_process_summaries(self, events):
+        return list(make_multi_process_summaries(events))
+
+    def test_empty(self):
+        self.assertEqual(self.make_multi_process_summaries([]), [])
+
+    def test_single_sw_start_stop_pair(self):
+        self.assertEqual(
+            self.make_multi_process_summaries([
+                StopwatchStart("p", "t", 1234, "f", None, None),
+                StopwatchStop("p", "t", 1534),
+            ]),
+            [Summary("f", None, 1, None, None, None, None, None, 300)],
         )
+
+    def test_sequential_sw_start_stop_pair_with_label(self):
+        self.assertEqual(
+            self.make_multi_process_summaries([
+                StopwatchStart("p", "t", 1234, "f", "label", None),
+                StopwatchStop("p", "t", 1434),
+                StopwatchStart("p", "t", 1534, "f", "label", None),
+                StopwatchStop("p", "t", 1934),
+            ]),
+            [Summary("f", "label", 2, 300, 100 * math.sqrt(2), 200, 300, 400, 600)],
+        )
+
+    def test_sw_summary(self):
+        self.assertEqual(
+            self.make_multi_process_summaries([
+                StopwatchSummary("p", "t", 42, "f", None, 12, 11, 10, 9, 8, 7, 6),
+            ]),
+            [
+                Summary("f", None, 12, 11, 10, 9, 8, 7, 6),
+            ],
+        )
+
+    def test_sw_summary_with_label(self):
+        self.assertEqual(
+            self.make_multi_process_summaries([
+                StopwatchSummary("p", "t", 42, "f", "label", 412, 411, 410, 49, 48, 47, 46),
+            ]),
+            [
+                Summary("f", "label", 412, 411, 410, 49, 48, 47, 46),
+            ],
+        )
+
+    def test_multiple_sw_summaries(self):
+        self.maxDiff = None
+        # There *can* be several 'StopwatchSummary' events with the same function_name and label,
+        # because we support reporting from several concatenated event files at once.
+        # But in that case we lose information about standard deviation and median.
+        for label in [None, "label"]:
+            self.assertEqual(
+                self.make_multi_process_summaries([
+                    StopwatchSummary("p", "t", 42, "f", label, 2, 11, 42, 10, 42, 11, 20),
+                    StopwatchSummary("p", "t", 42, "f", label, 4, 14, 42, 9, 42, 12, 40),
+                ]),
+                [
+                    Summary("f", label, 6, 13, None, 9, None, 12, 60),
+                ],
+            )
 
 
 @dataclasses.dataclass
@@ -264,12 +374,13 @@ class Summary:
     function_name: str
     label: Optional[str]
     executions_count: int
-    average_duration: int
-    duration_standard_deviation: int
-    min_duration: int
-    median_duration: int
-    max_duration: int
+    average_duration: Optional[int]
+    duration_standard_deviation: Optional[int]
+    min_duration: Optional[int]
+    median_duration: Optional[int]
+    max_duration: Optional[int]
     total_duration: int
+    # @todo (not needed by Laurent for now) Add summaries per process and per thread
 
     def json(self):
         d = collections.OrderedDict()
@@ -278,11 +389,16 @@ class Summary:
             d["label"] = self.label
         d["executions_count"] = self.executions_count
         if self.executions_count > 1:
-            d["average_duration_ms"] = to_ms(self.average_duration)
-            d["duration_standard_deviation_ms"] = to_ms(self.duration_standard_deviation)
-            d["min_duration_ms"] = to_ms(self.min_duration)
-            d["median_duration_ms"] = to_ms(self.median_duration)
-            d["max_duration_ms"] = to_ms(self.max_duration)
+            if self.average_duration is not None:
+                d["average_duration_ms"] = to_ms(self.average_duration)
+            if self.duration_standard_deviation is not None:
+                d["duration_standard_deviation_ms"] = to_ms(self.duration_standard_deviation)
+            if self.min_duration is not None:
+                d["min_duration_ms"] = to_ms(self.min_duration)
+            if self.median_duration is not None:
+                d["median_duration_ms"] = to_ms(self.median_duration)
+            if self.max_duration is not None:
+                d["max_duration_ms"] = to_ms(self.max_duration)
         d["total_duration_ms"] = to_ms(self.total_duration)
         return d
 
@@ -300,7 +416,10 @@ def merge_durations_and_summaries(a, b):
         merged_durations = durations_merged.setdefault(key, [])
         merged_durations += b_durations
 
-    summaries_merged = summaries_a + summaries_b
+    summaries_merged = dict(summaries_a)
+    for (key, b_summaries) in summaries_b.items():
+        merged_summaries = summaries_merged.setdefault(key, [])
+        merged_summaries += b_summaries
 
     return (durations_merged, summaries_merged)
 
@@ -328,14 +447,14 @@ class MultiThreadedDurationsExtractor:
                 (extractor.result for extractor in self.__extractors_per_thread.values()),
             )
         else:
-            return [{}, []]
+            return [{}, {}]
 
 
 class SingleThreadedDurationsExtractor:
     def __init__(self):
         self.__stack = []
         self.__durations = {}
-        self.__summaries = []
+        self.__summaries = {}
 
     def process(self, event):
         if event.__class__ == StopwatchStart:
@@ -347,17 +466,8 @@ class SingleThreadedDurationsExtractor:
             durations = self.__durations.setdefault((start_event.function_name, start_event.label), [])
             durations.append(duration)
         elif event.__class__ == StopwatchSummary:
-            self.__summaries.append(Summary(
-                function_name=event.function_name,
-                label=event.label,
-                executions_count=event.executions_count,
-                average_duration=event.average_duration,
-                duration_standard_deviation=event.duration_standard_deviation,
-                min_duration=event.min_duration,
-                median_duration=event.median_duration,
-                max_duration=event.max_duration,
-                total_duration=event.total_duration,
-            ))
+            summaries = self.__summaries.setdefault((event.function_name, event.label), [])
+            summaries.append(event)
         else:
             assert False
 
@@ -374,27 +484,8 @@ class ExtractDurationsTestCase(unittest.TestCase):
             extractor.process(event)
         return extractor.result[0]
 
-    def extract_summaries(self, events):
-        extractor = MultiThreadedDurationsExtractor()
-        for event in events:
-            extractor.process(event)
-        return extractor.result[1]
-
     def test_empty(self):
         self.assertEqual(self.extract_durations([]), {})
-        self.assertEqual(self.extract_summaries([]), [])
-
-    def test_summaries(self):
-        self.assertEqual(
-            self.extract_summaries([
-                StopwatchSummary("p", "t", 42, "f", None, 12, 11, 10, 9, 8, 7, 6),
-                StopwatchSummary("p", "t", 42, "f", "label", 412, 411, 410, 49, 48, 47, 46),
-            ]),
-            [
-                Summary("f", None, 12, 11, 10, 9, 8, 7, 6),
-                Summary("f", "label", 412, 411, 410, 49, 48, 47, 46),
-            ],
-        )
 
     def test_single_duration(self):
         self.assertEqual(
