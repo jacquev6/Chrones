d4bcbb1035d81feec208a120bf5489a90c5979f9
Vincent Jacques <vincent@vincent-jacques.net>
2022-12-01 10:17:26 +0000

    Enrich Python package metadata

--- publish.sh
+++ publish.sh
@@ -8,10 +8,15 @@ set -o errexit
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 
-# This script assumes that
+# @todo Use a dedicated virtualenv instead of assuming that
 #   pip install --upgrade build twine
 # has been run beforehand
 
+# @todo Bump version, before or after publishing
+# @todo Update changelog
+
+rm -rf dist  Chrones.egg-info
+
 python3 -m build
 
-# twine upload dist/*
+twine upload dist/*
--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@
 import setuptools
 
 
-version = "0.0.1"
+version = "0.0.2"
 
 setuptools.setup(
     name="Chrones",
     version=version,
+    description="Software development tool to visualize runtime statistics about your program and correlate them with its phases",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacquev6/Chrones",
+    author="Vincent Jacques",
+    author_email="vincent@vincent-jacuqes.net",
+    license="MIT",
     install_requires=open("requirements.txt").readlines(),
     entry_points={
         "console_scripts": [
